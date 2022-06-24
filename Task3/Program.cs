de1d7970b892eb1c897a4abd19f7850bad068ce6
Jane <spring1981@mail.ru>
2022-06-24 09:19:00 +0300

    Task3 updated

---
+++ Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача № 6
+
+int n1;
+
+Console.WriteLine("Input number: ");
+n1=Convert.ToInt32(Console.ReadLine());
+
+if(n1%2==0)
+{
+    Console.WriteLine("Число n1 - четное");
+}
+else
+{
+    Console.WriteLine("Число n1 - нечетное");
+}
+
