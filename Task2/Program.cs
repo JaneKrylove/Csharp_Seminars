369b8a9e4288f016432fb481fc44d5b7871b637d
Jane <spring1981@mail.ru>
2022-06-24 09:09:12 +0300

    Task2 updated

---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача № 4
+
+int n1, n2, n3, max;
+
+Console.WriteLine("Input first number: ");
+n1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+n2=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+n3=Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+{
+   max = n1;
+}
+else
+   max = n2;
+
+if (max > n3)
+{
+   Console.WriteLine("Maximum number "+ max);
+}
+else
+if (max < n3)
+{
+    max = n3;
+
+    Console.WriteLine("Maximum number " + max);
+}
