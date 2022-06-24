3b4801266b91dbcdade54763f7d1b977585ca5ed
Jane <spring1981@mail.ru>
2022-06-24 08:58:27 +0300

    Task1 updated

---
+++ Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача № 2
+
+int n1, n2, max, min;
+
+Console.WriteLine("Input first number: ");
+n1=Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Input second number: ");
+n2=Convert.ToInt32(Console.ReadLine());
+
+if (n1 > n2)
+{
+    max = n1;
+    min = n2;
+
+}
+else   
+    min = n1;
+    max = n2;
+
+    Console.WriteLine("Minumum number "+ min);
+    Console.WriteLine ("Maximum number " + max);
+
