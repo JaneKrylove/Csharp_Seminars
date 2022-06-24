8a205b16cf81fe0c9cbb12d6c9276ea333fb562b
Jane <spring1981@mail.ru>
2022-06-24 09:27:21 +0300

    Task4 updated

---
+++ Task4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Number 8
+
+int n, index=1;
+
+Console.WriteLine("Input number: ");
+n=Convert.ToInt32(Console.ReadLine());
+
+while (index<=n)
+{
+    if (index%2==0)
+    {
+        Console.WriteLine(index);
+    }
+    
+    index++;
+}   
+
