792b1b9a1505a3c24a37f9542199f59d595999ec
Jane <spring1981@mail.ru>
2022-06-30 07:36:31 +0300

    Task10 updated

---
+++ Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿int SecondDigital()
+{
+    int n1;
+
+    Console.WriteLine("Input number: ");
+    n1=Convert.ToInt32(Console.Readline());
+}
+    {
+        if(99<n1<100)
+    
+    int sot = firstbumber;
+    int dec = secondnumber;
+    int ed = thirdnumber;
+
+    int result = secondnumber;
+    return result;
+
+        Console.WriteLine("Вторая цифра "+ secondnumber);
+
+    }
+
+    
