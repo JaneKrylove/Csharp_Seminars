a0314cfbfc111b117f46b1576b905afc2a45ab30
Jane <spring1981@mail.ru>
2022-07-03 22:04:23 +0300

    Task15 updated

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,4 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+
+string number = Console.ReadLine();
+
+if (number != null) FindWeekdDay (number);
 
 
+void FindWeekdDay(String day)
+    
+{   
+    int n1 = Convert.ToInt32(day);
+    if (n1 <=0 || n1>7)
+        Console.WriteLine ("Are you crazy?");
+    else if (n1>5 && n1<=7)
+        Console.WriteLine ("Ура, это выходной!");
+
+    else Console.WriteLine ("Рабочий день");
+}
+
+    
