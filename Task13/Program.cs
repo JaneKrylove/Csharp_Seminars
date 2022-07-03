fa619a7f09f81d3284a2977cf702b84e3fe4f5e7
Jane <spring1981@mail.ru>
2022-07-03 21:25:02 +0300

    Task13 updated

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,33 @@
-﻿Здесь будет новая Задача
+﻿Console.WriteLine("Input number: ");
+string? n1 = Console.ReadLine();
+
+void FindThirdDigital(string number)
+{
+    int result = 0;
+    if(number.Length == 3)
+    {
+        result = Convert.ToInt32(number);
+        result = result % 10;
+        Console.WriteLine(result);
+      
+    } else if(number.Length < 3)
+    {
+        Console.WriteLine("третьей цифры нет");
+    } else if (number.Length > 3)
+    {
+        int count = number.Length;                                  //Если число более трёх знаков
+        int stepen = count -3;
+        result = Convert.ToInt16(number);
+        int delitel = (int)Math.Pow(10, stepen);
+        int newResult = result / delitel;
+
+        int lastResult = Convert.ToInt32(newResult % 10);
+
+        Console.WriteLine(lastResult); 
+    }
+
+     
+}
+
+FindThirdDigital(n1); 
 
