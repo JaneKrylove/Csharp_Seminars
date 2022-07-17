e9f668d13ebcb046694c2be82b2b2925d9864c63
Jane <spring1981@mail.ru>
2022-07-17 06:24:16 +0300

    task19 updated

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,8 +1,28 @@
-﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+﻿// Задача 19
 
-bull Palindrom (int x)
+//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
 
-Console.WriteLine("Введите число: ");
+bool Palindrom (int a)
+{
+    if(a/10000 == a % 10)
+        {
+            if(a / 1000 % 10 == a % 100 / 100);
+            return true;
+        }
 
+        else 
 
+        return false;
 
+}
+            
+    
+Console.WriteLine("Введите число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(Palindrom (number));
+        
+    
+    
+     
