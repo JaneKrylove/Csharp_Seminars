24fe058ae6bf5ba53c8d4335743087b8cd644c1b
Jane <spring1981@mail.ru>
2022-07-17 06:26:20 +0300

    task27 updated

---
+++ Task27/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 27: 
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int DigitalSum (int a)
+{
+    int sum = 0;
+
+   
+    while ( a > 0 )
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
+
+
+Console.WriteLine("Введите число ");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = DigitalSum (number);
+
+Console.WriteLine("Сумма цифр равна "+ result);
