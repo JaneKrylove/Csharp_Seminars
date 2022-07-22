6ea4872da668d7855617d7320e20c87dab105d04
Jane <spring1981@mail.ru>
2022-07-22 14:14:36 +0300

    task29 updated

--- Task29/Program.cs
+++ Task29/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: 
+// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+void Array[]
+{
+    int [] array = new array [numbers]
+    for (int i = 0; i < Array.Length; i++)
+
+    array[i] = 
+}
+
+
+
+
+Console.Write("Input number: ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Your array is: " + a );
